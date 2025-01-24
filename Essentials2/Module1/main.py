c81b2e139f90e3da89d525ff08aa7d2a85ca22bf
XhannAmatH <xhannamath@gmail.com>
2025-01-24 01:42:00 +0000

    Module 1 56%
    learning the basis of modules and writing my own
    man i want to eat a burrito

---
+++ Essentials2/Module1/main.py
@@ -0,0 +1,10 @@
+#   Author  :   XhannAmatH
+
+from module import sum1, prod1
+
+zeroes = [0 for i in range(5)]
+ones = [1 for i in range(5)]
+print(zeroes)
+print(ones)
+print(sum1(zeroes))
+print(prod1(ones))
---
+++ Essentials2/Module1/module.py
@@ -0,0 +1,28 @@
+#   Author  :   XhannAmatH
+#!/usr/bin/env python3
+__counter = 0
+
+
+def sum1(the_list):
+    global __counter
+    __counter += 1
+    the_sum = 0
+    for e in the_list:
+        the_sum+= e
+    return the_sum
+
+
+def prod1(the_list):
+    global __counter
+    __counter += 1
+    prod =1
+    for e in the_list:
+        prod*= e
+    return prod
+
+
+if __name__ == "__main__":
+    print("I prefer to be a module, but I can do some tests for you.")
+    my_list = [i+1 for i in range(5)]
+    print(sum1(my_list) == 15)
+    print(prod1(my_list) == 120)
